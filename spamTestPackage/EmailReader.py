af3db069d6050c4caaea72de96f5c619b7f7bcce
sharris124 <35932456+sharris124@users.noreply.github.com>
2019-04-08 09:44:16 -0700

    Added get_results to EmailReader and kNearestNeighbor

--- spamTestPackage/EmailReader.py
+++ spamTestPackage/EmailReader.py
@@ -53,160 +53,161 @@ def remove_common_words(dict):
 
     return dict
 
-"""
-read_emails_from_directory(directory)
 
-gets all emails in directory (dataset)
+'''
+    Simple solution to iterate through a directory and append
+    all .txt files to a list.
+    Great documentation here:
+    https://www.tutorialspoint.com/python3/os_listdir.htm
+    and here:
+    https://docs.python.org/3/library/os.path.html#os.path.join
 
-Input: directory (filedialog.askdirectory())
-Output: list of emails
-"""
+    Goes through each document word by word and adds them 
+    to a list (listOfWords)
+
+    Then it finally utilizes Counter to put it into a dictionary
+    that counts frequency of words and comes with a handy 
+    "most_common" feature.
+
+    At this point I'm not sure how many words to return
+    so I'm returning 500. It might be too much or too little, 
+    but we'll see once we expand it to machine learning.
+
+    Neil: I'm trying to make listOfPhrases contain 2 and 3 word phrases.
+    Will choose them indiscriminantly; just any 2 or 3 words that occur in a row
+'''
+
+
 def read_emails_from_directory(directory):
     emailList = []
     # Opens all files (including folders in files)
     for (path, _, files) in os.walk(directory):
         for file in files:
-            #if file.endswith('.txt'):  # not necessary but just is a safety measure
+            # if file.endswith('.txt'):  # not necessary but just is a safety measure
             emailList.append(os.path.join(path, file))
 
-    return emailList
+    bodyListOfWords = []
+    subjectListOfWords = []
+    bodyListOfPhrases = []
+    subjectListOfPhrases = []
+    for email in emailList:
 
-"""
-wordsAndPhrases(string str, int n)
+        current = open(email, encoding='latin-1')
+        lines = current.readlines()
 
-returns n most common words and phrases in string s
+        currentBodyListOfWords = []
+        currentSubjectListOfWords = []
 
-Output: n words, n 2-word phrases, n 3-word phrases, n 4-word phrases, n 5-word phrases
-"""
+        for i in lines:
+            # note: word is actually a list of all words in the line
+            word = i.split()
+            # check that word is not an empty list
+            if word:
+                if word[0] == "Subject:":
+                    currentSubjectListOfWords += word
+                else:
+                    currentBodyListOfWords += word
 
-# def wordsAndPhrases(string str, int n)
-#
-#     bodyListOfWords = []
-#     subjectListOfWords = []
-#     bodyListOfPhrases = []
-#     subjectListOfPhrases = []
-#     for email in emailList:
-#
-#         current = open(email, encoding='latin-1')
-#         lines = current.readlines()
-#
-#         currentBodyListOfWords = []
-#         currentSubjectListOfWords = []
-#
-#         for i in lines:
-#             # note: word is actually a list of all words in the line
-#             word = i.split()
-#             # check that word is not an empty list
-#             if word:
-#                 if word[0] == "Subject:":
-#                     currentSubjectListOfWords += word
-#                 else:
-#                     currentBodyListOfWords += word
-#
-#         # to keep track of phrases, I'll use a list that acts like a queue
-#         current2Word = ["", ""]
-#         current3Word = ["", "", ""]
-#         current4Word = ["", "", "", ""]
-#         current5Word = ["", "", "", "", ""]
-#
-#         """
-#         Get phrases for body
-#         """
-#         # listOfWords contains words seen in order in the email
-#         # Increment through all of them and keep track of phrases
-#         for word in currentBodyListOfWords:
-#             if word.isalpha() and word not in commonWordList:
-#                 # update current phrase seen
-#                 current2Word[0] = current2Word[1]
-#                 current2Word[1] = word
-#
-#                 current3Word[0] = current3Word[1]
-#                 current3Word[1] = current3Word[2]
-#                 current3Word[2] = word
-#
-#                 # add the words to listOfPhrases as a single string, with a space between them
-#                 bodyListOfPhrases.append(current2Word[0] + " " + current2Word[1])
-#                 bodyListOfPhrases.append(current3Word[0] + " " + current3Word[1] + " " + current3Word[2])
-#
-#         """
-#         Get phrases for subject
-#         """
-#
-#         """
-#         Note: It is interesting storing the phrases after removing stop words. Therefore, any phrase of 3 or 4 stopwords
-#         followed by a not stopword becomes the same. So I'm seeing ('   prescription ', 7) as a reflection that there were
-#         7 instances of 3 stopwords, then prescription, then a stop word.
-#
-#         We should test whether it is better to remove the stop words before or after storing the phrase.
-#         """
-#         # listOfWords contains words seen in order in the email
-#         # Increment through all of them and keep track of phrases
-#         for word in currentSubjectListOfWords:
-#             if word.isalpha() and word not in commonWordList:
-#                 # update current phrase seen
-#                 current2Word[0] = current2Word[1]
-#                 current2Word[1] = word
-#
-#                 current3Word[0] = current3Word[1]
-#                 current3Word[1] = current3Word[2]
-#                 current3Word[2] = word
-#
-#                 current4Word[0] = current4Word[1]
-#                 current4Word[1] = current4Word[2]
-#                 current4Word[2] = current4Word[3]
-#                 current4Word[3] = word
-#
-#                 current5Word[0] = current5Word[1]
-#                 current5Word[1] = current5Word[2]
-#                 current5Word[2] = current5Word[3]
-#                 current5Word[3] = current5Word[4]
-#                 current5Word[4] = word
-#
-#                 # add the words to listOfPhrases as a single string, with a space between them
-#                 subjectListOfPhrases.append(current2Word[0] + " " + current2Word[1])
-#                 subjectListOfPhrases.append(current3Word[0] + " " + current3Word[1] + " " + current3Word[2])
-#                 subjectListOfPhrases.append(current4Word[0] + " " + current4Word[1] + " " + current4Word[2]
-#                                             + " " + current4Word[3])
-#                 subjectListOfPhrases.append(current5Word[0] + " " + current5Word[1] + " " + current5Word[2]
-#                                             + " " + current5Word[3]+ " " + current5Word[4])
-#
-#         bodyListOfWords += currentBodyListOfWords
-#         subjectListOfWords += currentSubjectListOfWords
-#
-#     bodyWordFrequency = collections.Counter(bodyListOfWords)
-#     bodyWordFrequency = remove_common_words(bodyWordFrequency)
-#
-#     subjectWordFrequency = collections.Counter(subjectListOfWords)
-#     subjectWordFrequency = remove_common_words(subjectWordFrequency)
-#
-#     bodyPhraseFrequency = collections.Counter(bodyListOfPhrases)
-#
-#     subjectPhraseFrequency = collections.Counter(subjectListOfPhrases)
-#
-#     return bodyWordFrequency.most_common(500), subjectWordFrequency.most_common(500), bodyPhraseFrequency.most_common(
-#         500), subjectPhraseFrequency.most_common(500)
+        # to keep track of phrases, I'll use a list that acts like a queue
+        current2Word = ["", ""]
+        current3Word = ["", "", ""]
+        current4Word = ["", "", "", ""]
+        current5Word = ["", "", "", "", ""]
+
+        """
+        Get phrases for body
+        """
+        # listOfWords contains words seen in order in the email
+        # Increment through all of them and keep track of phrases
+        for word in currentBodyListOfWords:
+            if word.isalpha() and word not in commonWordList:
+                # update current phrase seen
+                current2Word[0] = current2Word[1]
+                current2Word[1] = word
+
+                current3Word[0] = current3Word[1]
+                current3Word[1] = current3Word[2]
+                current3Word[2] = word
+
+                # add the words to listOfPhrases as a single string, with a space between them
+                bodyListOfPhrases.append(current2Word[0] + " " + current2Word[1])
+                bodyListOfPhrases.append(current3Word[0] + " " + current3Word[1] + " " + current3Word[2])
+
+        """
+        Get phrases for subject
+        """
+
+        """
+        Note: It is interesting storing the phrases after removing stop words. Therefore, any phrase of 3 or 4 stopwords
+        followed by a not stopword becomes the same. So I'm seeing ('   prescription ', 7) as a reflection that there were
+        7 instances of 3 stopwords, then prescription, then a stop word.
+
+        We should test whether it is better to remove the stop words before or after storing the phrase.
+        """
+        # listOfWords contains words seen in order in the email
+        # Increment through all of them and keep track of phrases
+        for word in currentSubjectListOfWords:
+            if word.isalpha() and word not in commonWordList:
+                # update current phrase seen
+                current2Word[0] = current2Word[1]
+                current2Word[1] = word
+
+                current3Word[0] = current3Word[1]
+                current3Word[1] = current3Word[2]
+                current3Word[2] = word
+
+                current4Word[0] = current4Word[1]
+                current4Word[1] = current4Word[2]
+                current4Word[2] = current4Word[3]
+                current4Word[3] = word
+
+                current5Word[0] = current5Word[1]
+                current5Word[1] = current5Word[2]
+                current5Word[2] = current5Word[3]
+                current5Word[3] = current5Word[4]
+                current5Word[4] = word
+
+                # add the words to listOfPhrases as a single string, with a space between them
+                subjectListOfPhrases.append(current2Word[0] + " " + current2Word[1])
+                subjectListOfPhrases.append(current3Word[0] + " " + current3Word[1] + " " + current3Word[2])
+                subjectListOfPhrases.append(current4Word[0] + " " + current4Word[1] + " " + current4Word[2]
+                                            + " " + current4Word[3])
+                subjectListOfPhrases.append(current5Word[0] + " " + current5Word[1] + " " + current5Word[2]
+                                            + " " + current5Word[3] + " " + current5Word[4])
+
+        bodyListOfWords += currentBodyListOfWords
+        subjectListOfWords += currentSubjectListOfWords
+
+    bodyWordFrequency = collections.Counter(bodyListOfWords)
+    bodyWordFrequency = remove_common_words(bodyWordFrequency)
+
+    subjectWordFrequency = collections.Counter(subjectListOfWords)
+    subjectWordFrequency = remove_common_words(subjectWordFrequency)
+
+    bodyPhraseFrequency = collections.Counter(bodyListOfPhrases)
+
+    subjectPhraseFrequency = collections.Counter(subjectListOfPhrases)
+
+    return bodyWordFrequency.most_common(500), subjectWordFrequency.most_common(500), bodyPhraseFrequency.most_common(
+        500), subjectPhraseFrequency.most_common(500)
 
 
-if __name__ == "__main__":
+def get_result():
     # file opener
     tkinter.Tk().withdraw()
     directory = filedialog.askdirectory()
     result = read_emails_from_directory(directory)
+    # print("body words:", result[0])
+    # print("\n\nsubject words:", result[1])
+    # print("\n\nbody phrases:", result[2])
+    # print("\n\nsubject phrases:", result[3])
 
-    #get list of emails in directory
-    emailList = read_emails_from_directory(directory)
+    print("body words:", len(result[0]))
+    print("subject words:", len(result[1]))
+    print("body phrases:", len(result[2]))
+    print("subject phrases:", len(result[3]))
 
-    for email in emailList:
-        current = open(email, encoding='latin-1')
-        lines = current.readlines()
-        for line in lines:
-            print("l:" + line)
-
-    print("body words:", result[0])
-    print("subject words:", result[1])
-    print("body phrases:", result[2])
-    print("subject phrases:", result[3])
-
+    return result
     # directory input:
     #   C:\Users\Brad\PycharmProjects\EmailReader\spam
     # which I got from;
---
+++ spamTestPackage/knn.py
@@ -0,0 +1,91 @@
+import csv
+import numpy as np
+import pandas as pd
+from sklearn import neighbors
+from sklearn.model_selection import train_test_split, cross_validate
+
+
+from spamTestPackage.EmailReader import get_result
+
+
+def k_nearest_neighbor():
+
+    # df = pd.read_csv('')
+    # ham = 0 and spam = 1
+    df = pd.read_csv('data.csv', encoding='latin-1', names=['body_word', 'bw_freq', 'subject_words', 'sw_freq', 'body_phrases', 'bp_freq', 'subject_phrases', 'sp_freq', 'class'])
+    print(df)
+    x = np.array(df.drop(['class'], 1))
+    print(df)
+    y = np.array(df['class'])
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+    clf = neighbors.KNeighborsClassifier()
+    clf.fit(x_train, y_train)
+    accuracy = clf.score(x_test, y_test)
+    print(accuracy)
+
+# def k_nearest_neighbor(result, p, k):
+#
+#     distance = []
+#     euclidean_distance = 0
+#     for section in result:
+#         for group in section:
+#             for feature in group:
+#                 for s in p:
+#                     for g in s:
+#                         for f in g:
+#                             euclidean_distance += math.sqrt((feature[1] - f[1]) ** 2)
+#
+#                 # euclidean_distance += math.sqrt((feature[1]-feat[0])**2 + (feature[1]-feat[1])**2)
+#             distance.append((euclidean_distance, group))
+#     distance = sorted(distance)[:k]
+#
+#     # ham
+#     freq1 = 0
+#     # spam
+#     freq2 = 0
+#
+#     for d in distance:
+#         if d[1] == 0:
+#             freq1 += 1
+#         elif d[1] == 1:
+#             freq2 += 1
+#
+#     return 0 if freq1 > freq2 else 1
+#
+# def save_as_text(result):
+#     text_file = open("output.txt", "w")
+#     for i in result[0]:
+#         text_file.write("(" + i[0] + ",")
+#         text_file.write(i[1].__str__() + ") ")
+#     for i in result[1]:
+#         text_file.write("(" + i[0] + ",")
+#         text_file.write(i[1].__str__() + ") ")
+#     for i in result[2]:
+#         text_file.write("(" + i[0] + ",")
+#         text_file.write(i[1].__str__() + ") ")
+#     for i in result[3]:
+#         text_file.write("(" + i[0] + ",")
+#         text_file.write(i[1].__str__() + ") ")
+#     text_file.close()
+
+
+if __name__ == '__main__':
+    # ham = 0 and spam = 1
+    print("HAM")
+    ham = get_result()
+    print("SPAM")
+    spam = get_result()
+    with open('data.csv', 'w', newline='', encoding='latin-1') as csvfile:
+        writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for i in range(0, len(ham[0])):
+            writer.writerow([ham[0][i][0]] + [','] + [ham[0][i][1]] + [','] + [ham[1][i][0]] + [','] +
+                                [ham[1][i][1]] + [','] + [ham[2][i][0]] + [','] + [ham[2][i][1]] + [','] +
+                                [ham[3][i][0]] + [','] + [ham[3][i][1]] + [','] + 0)
+        for i in range(0, len(spam[0])):
+            writer.writerow([spam[0][i][0]] + [','] + [spam[0][i][1]] + [','] + [spam[1][i][0]] + [','] +
+                            [spam[1][i][1]] + [','] + [spam[2][i][0]] + [','] + [spam[2][i][1]] + [','] +
+                            [spam[3][i][0]] + [','] + [spam[3][i][1]] + [','] + 1)
+
+    k_nearest_neighbor()
